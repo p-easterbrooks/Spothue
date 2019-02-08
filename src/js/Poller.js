3f7d31a52349f02f0bcea936b86edb781eb6d505
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2019-02-08 14:00:50 -0500

    conform to linter

--- src/js/Poller.js
+++ src/js/Poller.js
@@ -1,24 +1,24 @@
 // =======================================================
 // Poller.js
 // =======================================================
-const EventEmitter = require('events');
+const EventEmitter = require('events')
 
 class Poller extends EventEmitter {
     /**
      * @param {int} timeout how long should we wait after the poll started?
      */
     constructor(timeout = 100) {
-        super();
-        this.timeout = timeout;
+        super()
+        this.timeout = timeout
     }
 
     poll() {
-        setTimeout(() => this.emit('poll'), this.timeout);
+        setTimeout(() => this.emit('poll'), this.timeout)
     }
 
     onPoll(cb) {
-        this.on('poll', cb);
+        this.on('poll', cb)
     }
 }
 
-module.exports = Poller;
+module.exports = Poller
--- src/js/SpotifyController.js
+++ src/js/SpotifyController.js
@@ -8,26 +8,26 @@ class SpotifyController {
             .split('&')
             .reduce(function (initial, item) {
                 if (item) {
-                    var parts = item.split('=');
-                    initial[parts[0]] = decodeURIComponent(parts[1]);
+                    var parts = item.split('=')
+                    initial[parts[0]] = decodeURIComponent(parts[1])
                 }
-                return initial;
-            }, {});
-        window.location.hash = '';
+                return initial
+            }, {})
+        window.location.hash = ''
     
         // Set token
-        let _token = hash.access_token;
+        let _token = hash.access_token
     
-        const authEndpoint = 'https://accounts.spotify.com/authorize';
-        const clientId = '451b6dddb0b24b1cbad3e071aa8fe9d0';
+        const authEndpoint = 'https://accounts.spotify.com/authorize'
+        const clientId = '451b6dddb0b24b1cbad3e071aa8fe9d0'
         const redirectUri = location.href
         const scopes = [
             'user-read-currently-playing',
             'user-read-playback-state'
-        ];
+        ]
         // If there is no token, redirect to Spotify authorization
         if (!_token) {
-            window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+            window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`
         }
     
         spotifyApi.setAccessToken(_token)
