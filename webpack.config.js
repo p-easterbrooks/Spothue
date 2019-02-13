095f0dc22a36ae7e9ba871ac17314dcfefa8e344
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2019-02-13 14:56:22 -0500

    use hue api code in bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/main.js',
+  mode: 'none',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 's.js'
+  },
+  node: {
+    dgram: 'empty'
   }
 };
